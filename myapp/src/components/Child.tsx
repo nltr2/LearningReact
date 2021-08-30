4a2b44d7d1876a6acff31e833fa199b607b57298
nltr <you@example.com>
2021-08-30 13:39:12 +0530

    calling props object oriented way

--- myapp/src/components/Child.tsx
+++ myapp/src/components/Child.tsx
@@ -3,11 +3,11 @@ import React from "react";
 interface ChildProps {
     color:string
 }
-/* export const Child =({color}:ChildProps)=>{
-    return <div >{color}</div>
+/* export const Child =(obj:ChildProps)=>{
+    return <div >{obj.color}</div>
 }
  */
 
-export const Child : React.FC<ChildProps>=({color}) =>{
-    return <div>{color}</div>
+export const Child : React.FC<ChildProps>=(obj) =>{
+    return <div>{obj.color}</div>
 }
