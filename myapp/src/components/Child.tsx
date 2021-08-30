207c84b2a187d87d9cd5497bfec4b9773b99e043
nltr <you@example.com>
2021-08-30 13:16:48 +0530

    creating first react functional component

--- myapp/src/components/Child.tsx
+++ myapp/src/components/Child.tsx
@@ -3,6 +3,11 @@ import React from "react";
 interface ChildProps {
     color:string
 }
-export const Child =({color}:ChildProps)=>{
+/* export const Child =({color}:ChildProps)=>{
+    return <div >{color}</div>
+}
+ */
+
+export const Child : React.FC<ChildProps>=({color}) =>{
     return <div>{color}</div>
 }
