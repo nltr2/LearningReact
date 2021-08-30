8c75faa92c3f1956dd7fc3ce1fa25507ca834c98
nltr <you@example.com>
2021-08-30 10:37:59 +0530

    first commit

---
+++ tsdemos/dist/welcome.js
@@ -0,0 +1,7 @@
+function greet(name) {
+    if (name === void 0) { name = 'nishant'; }
+    console.log('Hello ' + name);
+    console.log("Hello " + name);
+}
+greet();
+greet('admin');
---
+++ tsdemos/welcome.ts
@@ -0,0 +1,6 @@
+function greet(name='nishant'){
+    console.log('Hello '+name);
+    console.log(`Hello ${name}`);
+}
+greet();
+greet('admin');
