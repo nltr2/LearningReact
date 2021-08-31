857d2a884b889f4c7ef43e349700addb1aaac1fd
nltr <you@example.com>
2021-08-31 11:44:29 +0530

    using state

--- myapp/src/components/GuestList.tsx
+++ myapp/src/components/GuestList.tsx
@@ -1,15 +1,32 @@
 import React, { useState } from "react";
 
 const GuestList:React.FC=()=>{
-    //using state hooks
-    const [name,setName]= useState('admin')
+    //using state hooks : intialization
+    const [name,setName]= useState('admin');
+    const [guests,setGuests] = useState<string[]>([]);
+    const onClick = () =>{
+        setName('');
+        console.log(name);
+        setGuests([...guests,name]);
+    }
     return <div>
 
         <h3>Invited Guest List</h3>
+        <ul>
+
+         {guests.map((guest)=>(
+                <li key={guest}>{guest}</li>
+         )
         
+        )} 
+        {/*  {guests.map((guest) =>(
+               <li key={guest}>{guest}</li>
+           ))} */}
+      
+        </ul>
         <input type='text' value={name} onChange={(e)=>setName(e.target.value)}/>
         
-        <button>Add Guest</button>
+        <button onClick={onClick}>Add Guest</button>
         
         </div>
 }
