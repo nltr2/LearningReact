1fe291d31588a0aa443be054c7757814a538d4e9
nltr <you@example.com>
2021-08-31 11:55:30 +0530

    Added one more state to include city in guestlist

--- myapp/src/components/GuestList.tsx
+++ myapp/src/components/GuestList.tsx
@@ -3,28 +3,33 @@ import React, { useState } from "react";
 const GuestList:React.FC=()=>{
     //using state hooks : intialization
     const [name,setName]= useState('admin');
+    const [city,setCity]= useState('LA');
     const [guests,setGuests] = useState<string[]>([]);
     const onClick = () =>{
         setName('');
+        setCity('');
         console.log(name);
-        setGuests([...guests,name]);
+        var guest= name + ','+city;
+        setGuests([...guests,guest]);
     }
     return <div>
 
         <h3>Invited Guest List</h3>
         <ul>
+        {/* 2 ways to do it : one with return statemant and curly braces */}
+         {guests.map((guest)=>{
+             return <li key={guest}>{guest}</li>
+         }
+        )}  
 
-         {guests.map((guest)=>(
-                <li key={guest}>{guest}</li>
-         )
-        
-        )} 
+         {/* another using (  round brackets ,no return statement */}
         {/*  {guests.map((guest) =>(
                <li key={guest}>{guest}</li>
            ))} */}
       
         </ul>
         <input type='text' value={name} onChange={(e)=>setName(e.target.value)}/>
+        <input type='text' value={city} onChange={(e)=>setCity(e.target.value)}/>
         
         <button onClick={onClick}>Add Guest</button>
         
