f9ecdf5f2f32bfee637f3fc4f45c6c845a15edda
nltr <you@example.com>
2021-09-02 11:59:34 +0530

    using back button kind of functionality

--- myapp/src/components/routingDemo/UserDetails.tsx
+++ myapp/src/components/routingDemo/UserDetails.tsx
@@ -1,15 +1,19 @@
 import React from 'react'
-import { useParams } from 'react-router-dom'
+import {  useHistory, useParams } from 'react-router-dom'
 import UserNames from './UserNames'
 
 interface UserD{
     name:string
 }
  const UserDetails=() =>{
-
+    const history = useHistory()
+    const onClick=() =>{
+        history.push('/login')
+    }
     const {name} = useParams<UserD>()
     return (
         <div>
+            <button onClick={onClick}>go to login</button>
             <UserNames/>
             <p> User : {name}</p>
         </div>
